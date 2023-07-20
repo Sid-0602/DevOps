d9db01520b1a079193ee60c79e8b24d2a9a70b64
Siddhant_J <sidbnjadhav@gmail.com>
2023-07-21 00:16:22 +0530

    Python DS for Devops

---
+++ JSON,YML.Python/pyDevops.py
@@ -0,0 +1,40 @@
+
+print("This a DevOps course!")
+
+skill = "DevOps" #string varaible.
+print(skill)
+print("I am learning " + skill)
+
+#Integers: 
+
+print("*******Integers******")
+nums=10
+print(nums)
+
+
+#list
+print("This is a list")
+tools = ["Jenkins","Docker","K8s","Terraform",90]
+print(tools)
+print(tools[0])
+print(tools[-1])
+
+print("Printing range of list: ")
+print(tools[1:3])
+
+#tuple
+print("")
+print("THis is a tuple")
+tools1 = {"Jenkins","Docker","K8s"}
+print(tools1)
+
+#dictonary: It stores in key-value format. 
+print("")
+
+print("This is a dictonary")
+devops = {"skills":"DevOps",
+          "Year":2023,
+          "Tech": ""}
+print(devops)
+print(devops["skills"])
+print(devops["Year"])
